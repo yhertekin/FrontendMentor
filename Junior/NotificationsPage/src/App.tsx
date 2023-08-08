60ed186ba271a54605fc386afaf7b01a13dc5130
yhertekin <yhertekin@gmail.com>
2023-08-08 15:23:44 +0300

    fixed deployment

--- Junior/NotificationsPage/src/App.tsx
+++ Junior/NotificationsPage/src/App.tsx
@@ -1,36 +1,68 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import NotificationCard from "./components/NotificationCard";
 import { INotification } from "./interfaces/Notification";
 
 const initialData: INotification[] = [
     {
-        profilePicture: "",
-        name: "",
-        notificationText: "",
-        actionText: "",
-        time: "",
+        profilePicture: "./src/assets/images/avatar-mark-webber.webp",
+        name: "Mark Webber",
+        notificationText: "reacted to your recent post",
+        time: "1m",
+        actionText: "My first tournament today!",
         read: false,
-        message: "",
-        actionImage: "",
+    },
+    {
+        profilePicture: "./src/assets/images/avatar-angela-gray.webp",
+        name: "Angela Gray",
+        notificationText: "followed you",
+        time: "5m",
+        read: false,
+    },
+    {
+        profilePicture: "./src/assets/images/avatar-jacob-thompson.webp",
+        name: "Jacob Thompson",
+        notificationText: "has joined your group",
+        actionText: "Chess Club",
+        time: "1 day",
+        read: false,
+    },
+    {
+        profilePicture: "./src/assets/images/avatar-rizky-hasanuddin.webp",
+        name: "Rizky Hasanuddin",
+        notificationText: "sent you a private message",
+        message:
+            "Hello, thanks for seetting up the Chess Clu. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.",
+        time: "5 days",
+        read: true,
+    },
+    {
+        profilePicture: "./src/assets/images/avatar-kimberly-smith.webp",
+        name: "Kimberly Smith",
+        notificationText: "commented on your picture",
+        actionImage: "./src/assets/images/image-chess.webp",
+        time: "1 week",
+        read: true,
+    },
+    {
+        profilePicture: "./src/assets/images/avatar-nathan-peterson.webp",
+        name: "Nathan Peterson",
+        notificationText: "reacted to your recent post",
+        actionText: "5 end-game strategies to increase your win rate",
+        time: "2 weeks",
+        read: true,
+    },
+    {
+        profilePicture: "./src/assets/images/avatar-anna-kim.webp",
+        name: "Anna Kim",
+        notificationText: "left the group",
+        actionText: "Chess Club",
+        time: "2 weeks",
+        read: true,
     },
 ];
 
 function App() {
     const [data, setData] = useState<INotification[]>(initialData);
-
-    const fetchNotificationData = () => {
-        fetch("./data.json")
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                setData(data);
-            });
-    };
-
-    useEffect(() => {
-        fetchNotificationData();
-    }, []);
 
     const updateData = () => {
         setData((prevState) => prevState.map((item) => ({ ...item, read: true })));
