cf807ada8fbdefd1910f2cad07bf1b4ab4ef69b0
yhertekin <yhertekin@gmail.com>
2023-10-18 00:15:31 +0300

    update

--- Junior/IntroSectionWithDropdownNavigation/src/app.js
+++
@@ -1,6 +0,0 @@
-const toggleMenuButton = document.querySelector(".toggle-menu");
-const menu = document.querySelector(".menu");
-
-toggleMenuButton.addEventListener("click", function () {
-	menu.classList.toggle("hidden");
-});
--- Junior/IntroSectionWithDropdownNavigation/tailwind.config.js
+++ Junior/IntroSectionWithDropdownNavigation/tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{html,js}"],
+	content: ["*.{html,js}"],
 	theme: {
 		extend: {
 			colors: {
-				white: "hsl(0, 0%, 98%)",
-				gray: "hsl(0, 0%, 41%)",
-				black: "hsl(0, 0%, 8%)",
+				"almost-white": "hsl(0, 0%, 98%)",
+				"medium-gray": "hsl(0, 0%, 41%)",
+				"almost-black": "hsl(0, 0%, 8%)",
 			},
 			fontFamily: {
 				epilogue: ["Epilogue", "sans-serif"],
