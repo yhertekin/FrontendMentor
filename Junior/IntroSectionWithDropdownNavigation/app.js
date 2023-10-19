71bbe9d87656c086e22cd3f3b2c191dd3a39958a
yhertekin <yhertekin@gmail.com>
2023-10-19 23:21:53 +0300

    update

--- Junior/IntroSectionWithDropdownNavigation/app.js
+++ Junior/IntroSectionWithDropdownNavigation/app.js
@@ -1,12 +1,11 @@
 const toggleMenu = document.querySelectorAll(".toggle-menu");
-const menu = document.querySelector(".menu");
+const menu = document.querySelector(".menu-mobile");
 
 toggleMenu.forEach((btn) =>
-    btn.addEventListener("click", () => {
-        menu.classList.toggle("translate-x-full");
-        menu.classList.toggle("translate-x-0");
-        menu.classList.toggle("opacity-0");
-    })
+	btn.addEventListener("click", () => {
+		menu.classList.toggle("translate-x-full");
+		menu.classList.toggle("translate-x-0");
+	})
 );
 
 const features = document.querySelector(".features");
@@ -14,12 +13,12 @@ const featuresMenu = document.querySelector(".features-menu");
 const featuresIcon = document.querySelector(".features-icon");
 
 features.addEventListener("click", () => {
-    featuresMenu.classList.toggle("hidden");
-    if (featuresMenu.classList.contains("hidden")) {
-        featuresIcon.src = "/assets/icon-arrow-down.svg";
-    } else {
-        featuresIcon.src = "/assets/icon-arrow-up.svg";
-    }
+	featuresMenu.classList.toggle("hidden");
+	if (featuresMenu.classList.contains("hidden")) {
+		featuresIcon.src = "/assets/icon-arrow-down.svg";
+	} else {
+		featuresIcon.src = "/assets/icon-arrow-up.svg";
+	}
 });
 
 const company = document.querySelector(".company");
@@ -27,10 +26,10 @@ const companyMenu = document.querySelector(".company-menu");
 const companyIcon = document.querySelector(".company-icon");
 
 company.addEventListener("click", () => {
-    companyMenu.classList.toggle("hidden");
-    if (companyMenu.classList.contains("hidden")) {
-        companyIcon.src = "/assets/icon-arrow-down.svg";
-    } else {
-        companyIcon.src = "/assets/icon-arrow-up.svg";
-    }
+	companyMenu.classList.toggle("hidden");
+	if (companyMenu.classList.contains("hidden")) {
+		companyIcon.src = "/assets/icon-arrow-down.svg";
+	} else {
+		companyIcon.src = "/assets/icon-arrow-up.svg";
+	}
 });
