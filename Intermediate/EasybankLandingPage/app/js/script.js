2ca67250923e04d5845a377fc29998e9a47c5837
yhertekin <yhertekin@gmail.com>
2024-01-25 17:56:55 +0300

    update mobile menu

--- Intermediate/EasybankLandingPage/app/js/script.js
+++ Intermediate/EasybankLandingPage/app/js/script.js
@@ -1,15 +1,25 @@
+const body = document.querySelector("body")
 const btnHamburger = document.querySelector("#btnHamburger")
 const header = document.querySelector(".header")
 const overlay = document.querySelector(".overlay")
+const fadeElems = document.querySelectorAll(".has-fade")
 
 btnHamburger.addEventListener("click", function () {
     if (header.classList.contains("open")) {
-        overlay.classList.remove("fade-in")
-        overlay.classList.add("fade-out")
         header.classList.remove("open")
+        body.classList.remove("noscroll")
+        fadeElems.forEach((elem) => {
+            elem.classList.remove("fade-in")
+            elem.classList.add("fade-out")
+        })
     } else {
-        overlay.classList.add("fade-in")
-        overlay.classList.remove("fade-out")
+        // overlay.classList.add("fade-in")
+        // overlay.classList.remove("fade-out")
+        body.classList.add("noscroll")
         header.classList.add("open")
+        fadeElems.forEach((elem) => {
+            elem.classList.add("fade-in")
+            elem.classList.remove("fade-out")
+        })
     }
 })
